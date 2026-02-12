1f6e1999a990e351741c959fa2f6b747ece3ef47
zhangao <2212433186@qq.com>
2026-02-12 17:34:00 +0800

    XML请求demo

--- XMLHttpRequest/test.js
+++ XMLHttpRequest/test.js
@@ -1,44 +1,52 @@
 function GetWebData(URL) {
+    // 1. 新建 XMLHttpRequest 请求对象
     const xhr = new XMLHttpRequest();
   
     xhr.onreadystatechange = function () {
+      // 2. 处理请求结果
       switch (xhr.readyState) {
-        case 0:
-          console.log("0: UNSENT（刚创建，还没 open）");
+        case 0:  // 请求未初始化
+          console.log("0: UNSENT(刚创建，还没 open)");
           break;
-        case 1:
-          console.log("1: OPENED（已 open）");
+        case 1: // OPENED打开
+          console.log("1: OPENED(已 open)");
           break;
-        case 2:
-          console.log("2: HEADERS_RECEIVED（响应头到）");
+        case 2: // HEADERS_RECEIVED响应头接收完成
+          console.log("2: HEADERS_RECEIVED(响应头到了)");
           break;
-        case 3:
-          console.log("3: LOADING（响应体下载中）");
+        case 3: // LOADING正在接收响应体
+          console.log("3: LOADING(响应体下载中)");
           break;
-        case 4:
-          console.log("4: DONE（完成）");
+        case 4: // DONE接收完成
+          console.log("4: DONE(完成)");
           if (xhr.status === 200 || xhr.status === 304) {
             console.log("响应内容：", xhr.responseText);
           } else {
-            console.log("请求失败，status=", xhr.status);
+            console.log("请求失败,status=", xhr.status);
           }
           break;
       }
     };
   
-    xhr.ontimeout = function () {
+    // 3. 处理请求超时
+    xhr.ontimeout = function (e) {
       console.log("请求超时");
     };
   
-    xhr.onerror = function () {
+    // 4. 处理请求错误
+    xhr.onerror = function (e) {
       console.log("网络错误/被浏览器拦截（可能跨域）");
     };
   
-    xhr.open("GET", URL, true);
-    xhr.timeout = 3000;
-    xhr.responseType = "text";
+    // 5. 打开请求
+    xhr.open("GET", URL, true);  // 创建一个 Get 请求, 采用异步
+
+    // 6. 配置参数
+    xhr.timeout = 3000;  // 设置 xhr 请求的超时时间
+    xhr.responseType = "text"; // 设置响应返回的数据格式
     xhr.setRequestHeader("X_TEST", "time.geekbang");
   
+    // 7. 发送请求
     xhr.send();
   }
   
