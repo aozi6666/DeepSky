b14a770ed2d9ef87c01517b7dd103366fa72fe2e
zhangao <2212433186@qq.com>
2026-03-26 14:19:28 +0800

    3.26

--- deepClone/objectDeepClone.js
+++ deepClone/objectDeepClone.js
@@ -1,28 +1,30 @@
-function deepClone(obj, map = new WeakObj()){
+function deepClone(obj, map = new WeakMap()){
+    // 1. 基础数据类型，直接返回（包括Null）
+    if(typeof obj !== 'object' || obj === null){
+        return obj;
+    }
 
-    // 1) 基本数据类型，直接返回
-    if(typeof obj !== 'object' || obj === null) return;
-
-    // 自身引用
-    if(map.has(obj)) {
+    // 2. 处理循环引用
+    if(map.has(obj)){
         return map.get(obj);
     }
 
-    // 2) 创建 新对象/数组
-    const newObj = Array.isArray ? [] : {};
 
-    // 3) 
-    map.set(obj, newObj);
+    //3.新建 对象，用于拷贝
+    const newObj = Array.isArray(obj) ? [] : {};
 
-    // 递归循环 深拷贝
-    for(let key in map){
-        if(Object.prototype.hasOwnProperty.call(obj, key)) {
-            newObj[key] = deepClone(obj[key], map);
-        }
+    // 4. 遍历自身属性
+    for(let key in obj){
+         // 使用原型链上的方法,进行赋值
+         if(Onject.prototype.hasOwnPrototype.call(obj, key)){
+            // 递归赋值
+            new[key] = deepClone(obj[key], map);
+         }
     }
 
-    // 返回新对象
+    // 5. 返回
     return newObj;
+
 }
 
 const obj = {
